42a36e1d47f79f114b7be19915d8b684711e4a0a
applexi <aprille.xi@toolsforhumanity.com>
2026-07-07 14:18:38 -0700

    test

--- enclave/src/main.rs
+++ enclave/src/main.rs
@@ -1,11 +1,16 @@
 mod rng;
 
+use std::{thread, time::Duration};
+
 fn main() -> std::io::Result<()> {
     let rng_configured = rng::configure_rng()?;
-    if rng_configured {
-        println!("configured rng");
-    } else {
-        println!("rng already configured");
+    loop {
+        if rng_configured {
+            println!("configured rng");
+        } else {
+            println!("rng already configured");
+        }
+        thread::sleep(Duration::from_secs(5));
     }
     Ok(())
 }
