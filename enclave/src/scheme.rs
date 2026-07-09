5f7c08b89ed16c015d341f5910817bcb2d3167a2
applexi <aprille.xi@toolsforhumanity.com>
2026-07-09 13:50:26 -0700

    Added correctness test for TEE scheme

--- enclave/src/scheme.rs
+++ enclave/src/scheme.rs
@@ -24,3 +24,56 @@ pub fn enclave_session<R: TryCryptoRng> (arithmetic: &ArithmeticSharing, binary:
         }).collect();
     Ok(shares)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use getrandom::SysRng;
+
+    fn session_correct() {
+        let arithmetic = ArithmeticSharing::new();
+        let binary = BinarySharing::new();
+        let mut rng = SysRng;
+        let shares = enclave_session(&arithmetic, &binary, &mut rng)
+            .expect("Function enclave_session returned an error");
+        // Function enclave_session should return N shares
+        assert!(shares.len() == DEFAULT_N);
+
+        // Test arithmetic reconstruction is correct
+        let arith_fold = shares
+            .iter()
+            .fold(0 as u64, |acc, (ct_i, _)| acc.wrapping_add(*ct_i));
+        let arith_shares: Vec<u64>   = shares
+            .iter()
+            .map(|(ct_i, _)| *ct_i)
+            .collect();
+        let arith_recon = arithmetic.reconstruct(&arith_shares);
+        assert!(arith_fold == arith_recon);
+
+        let num_bits = ArithShare::BITS;
+        let mut binary_recon: u64 = 0;
+        for j in 0..num_bits {
+            // Test each binary reconstruction is correct
+            let binary_fold = shares  
+                .iter()
+                .fold(false, |acc, (_, ctbits_i)| acc ^ ctbits_i[j as usize]);
+            let binary_shares : Vec<bool> = shares
+                .iter()
+                .map(|(_, ctbits_i)| ctbits_i[j as usize])
+                .collect();
+            let binary_recon_i = binary.reconstruct(&binary_shares);
+            assert!(binary_fold == binary_recon_i);
+            binary_recon = binary_recon | ((binary_recon_i as u64) << j);
+        }
+        
+        // Test correlation is correct
+        assert!(arith_recon == binary_recon);
+    }
+
+    #[test]
+    fn batch_test() {
+        for _ in 0..20 {
+            session_correct();
+        }
+    }
+}
