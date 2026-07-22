5afd6afe92a7c4f3ea5b933e02769dd1c86167d1
applexi <aprille.xi@toolsforhumanity.com>
2026-07-22 11:16:17 -0700

    interface aesthetics

--- host/src/main.rs
+++ host/src/main.rs
@@ -79,11 +79,11 @@ async fn main() -> Result<(), Error> {
             }
             RequestType::Verify => {
                 let Some(attest_path) = input.attest_path else {
-                    warn!("ERROR: Attestation path required for verification");
+                    warn!("\nERROR: Attestation path required for verification");
                     continue
                 };
                 let Some((attestation_blob, session_id, is_bin)) = attestation_from_path(&attest_path).ok() else {
-                    warn!("ERROR: Attestation path is wrong");
+                    warn!("\nERROR: Attestation path is wrong");
                     continue
                 };
                 let mut session_id = match session_id {
@@ -95,41 +95,41 @@ async fn main() -> Result<(), Error> {
                 let session_input = SessionInput{ session_id, pcrs };
                 if let Some(signed_path) = input.signed_shares_path && let Some(enc_path) = input.enc_shares_path {
                     let Some((signed_shares, enc_shares)) = shares_from_path(&signed_path, &enc_path).ok() else {
-                        warn!("ERROR: Signed shares path and/or encrypted shares path are wrong");
+                        warn!("\nERROR: Signed shares path and/or encrypted shares path are wrong");
                         continue
                     };
                     if is_bin {
                         if let Err(e) = verify_session(&attestation_blob, &signed_shares, &enc_shares, &session_input) {
-                            warn!("FAILED: Verification failed with error {e:?}");
+                            warn!("\nFAILED: Verification failed with error {e:?}");
                             continue
                         }
                     } else {
-                        info!("Note: Attestation path given is (.json) not (.bin). Can only assume attestation was valid, and verify enclave scheme.");
+                        info!("\nNote: Attestation path given is (.json) not (.bin). Can only assume attestation was valid, and verify enclave scheme.");
                         let attestation: AttestationDoc = serde_json::from_slice(&attestation_blob)?;
                         if let Err(e) = verify_enclave_attestation(&attestation, &signed_shares, &enc_shares, &session_input) {
-                            warn!("FAILED: Verification failed with error {e:?}");
+                            warn!("\nFAILED: Verification failed with error {e:?}");
                             continue
                         }
                     }
                 } else if is_bin {
-                    info!("Note: Only given an attestation path with no shares paths. Will only verify if attestation is valid.");
+                    info!("\nNote: Only given an attestation path with no shares paths. Will only verify if attestation is valid.");
                     let attestation = pontifex::SecureModule::parse_raw_attestation_doc(&attestation_blob)?;
                     if let Err(e) = verify_aws_attestation(&attestation_blob, &attestation) {
-                        warn!("FAILED: Verification failed with error {e:?}");
+                        warn!("\nFAILED: Verification failed with error {e:?}");
                         continue
                     }
                 } else {
-                    warn!("No verification could be done with the parameters given.");
+                    warn!("\nWARNING: No verification could be done with the parameters given.");
                     info!("For full verification: --attestation (.bin) and --signed-shares (.cbor) and --enc-shares (.cbor)");
                     info!("For valid AWS attestation: --attestation (.bin)");
                     info!("For just enclave scheme: --attestation (.json) and --signed-shares (.cbor) and --enc-shares (.cbor)")
                 }
-                info!("SUCCESS: Verification successful!");
+                info!("\nSUCCESS: Verification successful!");
             }
             Quit => break,
         }
     }
     println!("==============================================================================================================");
-    info!("Connection broken.");
+    info!("\nConnection broken.");
     Ok(())
 }
